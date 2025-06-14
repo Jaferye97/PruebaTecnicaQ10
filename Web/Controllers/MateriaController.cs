26ccd8f6219efe6bc5c0847165469bc92cbac82f
Yeimer Jaramillo <andresjara0897@hotmail.com>
2025-06-14 08:43:02 -0500

    Se agrega vista de visualizar listado de materias.

---
+++ Web/Controllers/MateriaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Interface.Services;
+
+namespace Web.Controllers
+{
+    public class MateriaController : Controller
+    {
+        private readonly ILogger<MateriaController> _logger;
+        private readonly IMateriaService _materiaService;
+
+        public MateriaController(ILogger<MateriaController> logger, IMateriaService materiaService)
+        {
+            _logger = logger;
+            _materiaService = materiaService;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var materias = await _materiaService.GetAllAsync();
+            return View(materias);
+
+        }
+    }
+}
