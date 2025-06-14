b40e471071070ca172d68f8e103bf025814f20c5
Yeimer Jaramillo <andresjara0897@hotmail.com>
2025-06-14 09:42:24 -0500

    Se agrega configuración de la capa de negocio y la capa de acceso a base de datos para la entidad Semestre.

---
+++ Service/Services/SemestreService.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Model;
+using Repository.Context;
+using Repository.Interface.Repositories;
+using Service.Interface.Services;
+
+namespace Service.Services
+{
+    public class SemestreService : BaseService<Semestre, int, ISemestreRepository>, ISemestreService
+    {
+        private readonly ISemestreRepository _SemestreRepository;
+
+        public SemestreService(EntityDbContext context, ISemestreRepository repository) : base(context, repository)
+        {
+            this._SemestreRepository = repository;
+        }
+    }
+}
